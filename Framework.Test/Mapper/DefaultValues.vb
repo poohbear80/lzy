226c4ebfcb27408879e7935bff967724c621554c
unknown <pan@PETTER_LAT.itaslan.infotjenester.no>
2014-12-02 10:40:52 +0100

    Fixed mapping for decimal. Also fixed default template to reflect new structure

---
+++ Framework.Test/Mapper/DefaultValues.vb
@@ -0,0 +1,21 @@
+﻿Imports NUnit.Framework
+Imports Microsoft.SqlServer.Management.Smo
+
+
+Namespace Mapper
+    <TestFixture> Public Class DefaultValues
+
+        <Test> Public Sub DefaultValuesIsWrittenCorrectly()
+            Dim col As New Column()
+
+            col.DataType = DataType.Bit
+
+
+
+        End Sub
+
+
+
+
+    End Class
+End Namespace
--- Mapper/JsTemplates/DataAccess.js
+++ Mapper/JsTemplates/DataAccess.js
@@ -1,6 +1,9 @@
-result.StartBlock("Namespace DataAccess", []);
+result.WriteComment("ReSharper disable once CheckNamespace")
+result.StartBlock("Namespace DataAccess");
+result.WriteLine("");
 result.StartBlock("Friend Class {0}DataAccess", [data.TableName]);
-    
-
+result.WriteLine("Inherits Itas.Core.DataAccessBase.CustomerDb")
+result.WriteLine("");
 result.EndBlock("End Class");
+result.WriteLine("");
 result.EndBlock("End Namespace");
