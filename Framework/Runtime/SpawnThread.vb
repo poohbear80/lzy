b77557640123c09acc7ba5fa30a12ba657812db3
unknown <pan@PETTER_LAT.itaslan.infotjenester.no>
2014-12-09 13:25:46 +0100

    Fix for setting things locally in multithreading

--- Framework/Runtime/SpawnThread.vb
+++ Framework/Runtime/SpawnThread.vb
@@ -85,41 +85,5 @@ Namespace Runtime
                    End Sub
 
         End Function
-
-
-
-        Public Class ProxyStorage(Of TKey, TValue)
-            Inherits Dictionary(Of TKey, TValue)
-
-            Private ReadOnly _Origin As Dictionary(Of TKey, TValue)
-
-            Public Sub New(origin As Dictionary(Of TKey, TValue))
-                _Origin = origin
-            End Sub
-
-            Public Overloads Function ContainsKey(key As TKey) As Boolean
-                If MyBase.ContainsKey(key) Then
-                    Return True
-                Else
-                    Return _Origin.ContainsKey(key)
-                End If
-            End Function
-
-            Default Public Overloads Property Item(key As TKey) As TValue
-                Get
-                    If MyBase.ContainsKey(key) Then
-                        Return MyBase.Item(key)
-                    Else
-                        Return _Origin(key)
-                    End If
-                End Get
-                Set(value As TValue)
-                    MyBase.Item(key) = value
-                End Set
-            End Property
-
-
-        End Class
-
     End Class
-End NameSpace
+End Namespace
