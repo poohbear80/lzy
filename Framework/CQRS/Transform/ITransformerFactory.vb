a7a3ffc8db88cb1ea0b842fdc83561494cdc85cf
PEtter Ekrann <petter@infotjenester.no>
2014-12-22 15:34:57 +0100

    TEST

--- Framework/CQRS/Transform/ITransformerFactory.vb
+++ Framework/CQRS/Transform/ITransformerFactory.vb
@@ -3,5 +3,6 @@
 Namespace CQRS.Transform
     Public Interface ITransformerFactory
         Function GetTransformer(ByVal action As IAmAnAction, ByVal ent As Object) As ITransformEntityToDto
+        Function SortingFunc() As Comparison(Of Object)
     End Interface
 End Namespace
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 lzy
 ===
+
+This is a very tiny application framework. 
+
+Used in production since 2008.
+
+Contains
+
+- Mapper
+- Classfactory
+- Dataccess layer
+- CQRS handler
+
