f8b4d008224fda1bf396c7734f79178988ade0bb
PEtter Ekrann <pan@infotjenester.no>
2015-04-20 08:53:58 +0200

    LZY-2 #time 2h #comment Testing smartcommit

--- Elastic/LazyFramework.Elastic/ElasticProvider.vb
+++ Elastic/LazyFramework.Elastic/ElasticProvider.vb
@@ -12,25 +12,4 @@ Public Class Provider
     Public Function CreateConnection(connectionInfo As Data.ServerConnectionInfo) As IDbConnection Implements IDataAccessProvider.CreateConnection
 
     End Function
-<<<<<<< Updated upstream
 End Class
-=======
-End Class
-
-Public Class CommandTextGenerator
-
-
-    Public Shared Function Insert(template) As String
-
-    End Function
-
-    Public Shared Function Read(type As String, params As ) As String
-
-    End Function
-
-    Public Shared Function Search()
-
-    End Function
-
-End Class
->>>>>>> Stashed changes
