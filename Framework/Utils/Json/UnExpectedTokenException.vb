6a0bab437e4dc914bef639f67cf504a246ef845e
PEtter Ekrann <petter@ekrann.no>
2015-06-01 16:00:54 +0200

    Remove files from JSON. Will be added as submodule

--- Framework/Utils/Json/UnExpectedTokenException.vb
+++
@@ -1,9 +0,0 @@
-Namespace Utils.Json
-    Public Class UnExpectedTokenException
-        Inherits Exception
-
-        Public Sub New(ByVal current As Char)
-            MyBase.New(current)
-        End Sub
-    End Class
-End NameSpace
