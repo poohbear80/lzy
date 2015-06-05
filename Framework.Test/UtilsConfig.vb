c8eaf320af945a1e4006caf9745bf84bf8640578
PEtter Ekrann <petter@ekrann.no>
2015-06-05 13:20:43 +0200

    Added utils for Reading config files from Json. Added test for this

---
+++ Framework.Test/UtilsConfig.vb
@@ -0,0 +1,19 @@
+﻿Imports NUnit.Framework
+
+<TestFixture> Public Class UtilsConfig
+
+    <Test> Public Sub LoadConfigFromFile()
+
+        Dim conf = Utils.Config.Reader.Load(Of SystemConfig)
+
+        Assert.AreEqual("ABC", conf.StringValue)
+
+    End Sub
+
+    Public Class SystemConfig
+        Public StringValue As String
+        Public IntValue As Integer
+        Public DoubleValue As Double
+    End Class
+
+End Class
---
+++ Framework/Utils/Config/Reader.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+
+Namespace Utils.Config
+    Public Class Reader
+
+        Public Shared Function Load(Of T As New)() As T
+            Return Load(Of T)(GetType(T).Name & ".json")
+        End Function
+
+        Public Shared Function Load(Of T As New)(path As String) As T
+            Dim stream = System.IO.File.OpenRead(path)
+            Return Load(Of T)(New StreamReader(stream))
+        End Function
+
+        Public Shared Function Load(Of T As New)(stream As StreamReader) As T
+
+            Return Utils.Json.Reader.StringToObject(Of T)(stream)
+
+        End Function
+
+    End Class
+End Namespace
+
